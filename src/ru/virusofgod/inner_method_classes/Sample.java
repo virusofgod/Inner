63426e5f92beb331f826a338fbf0d2b32daf575e
Gulyaev Alexander <virusofgod@yandex.ru>
2022-08-25 16:20:09 +0400

    Lesson 18 Example 2method inner classes

--- src/ru/virusofgod/inner_method_classes/Sample.java
+++ src/ru/virusofgod/inner_method_classes/Sample.java
@@ -1,4 +1,35 @@
 package ru.virusofgod.inner_method_classes;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sample {
+
+
+
+    public void createNewPerson(String name){
+        String nameFIO=name;
+
+        class GenericName{
+            List<String> names = new ArrayList<String>();
+            public GenericName(){
+                for(String str: nameFIO.split(" ")){
+                    names.add(str);
+
+                }
+            }
+            public String getFirstName(){
+                return names.get(0);
+            }
+
+            public String getLastName(){
+                return names.get(names.size()-1);
+
+            }
+
+        }
+        GenericName gname = new GenericName();
+        System.out.println("Person: " + gname.getFirstName().charAt(0) + "." + gname.getLastName());
+    }
 }
