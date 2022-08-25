d708577c8281d3659b76242448f521103d2e602f
Gulyaev Alexander <virusofgod@yandex.ru>
2022-08-25 12:41:16 +0400

    Lesson 18 example 0 static nested classes

---
+++ src/ru/virusofgod/static_nested_class/Boeing737.java
@@ -0,0 +1,23 @@
+package ru.virusofgod.static_nested_class;
+
+public class Boeing737 {
+
+    private int manufactureYear;
+    private static int maxPassengersCount = 300;
+
+    public Boeing737(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public static class Drawing {
+
+        public static int getMaxPassengersCount() {
+
+            return maxPassengersCount;
+        }
+    }
+}
---
+++ src/ru/virusofgod/static_nested_class/Main.java
@@ -0,0 +1,10 @@
+package ru.virusofgod.static_nested_class;
+
+public class Main {
+    public static void main(String[] args) {
+        Boeing737.Drawing drawing = new Boeing737.Drawing();
+
+        System.out.println(drawing.getMaxPassengersCount());
+
+    }
+}
